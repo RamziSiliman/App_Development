e0e3edb8097c9a2bc3c7c172681d73a322facc8f
Nankunda1 <137884302+Nankunda1@users.noreply.github.com>
2023-12-20 11:23:47 +0300

    Update urls.py
    deleted spaces in the file

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,6 +1,5 @@
 from . import views
 from django.urls import path
-
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('token/', views.CustomTokenObtainPairView.as_view()),
