1849907c69225b9b0748c19f5f813bbda95b732a
Nankunda1 <137884302+Nankunda1@users.noreply.github.com>
2023-12-20 11:20:26 +0300

    Update manage.py
    added in the comment

--- backend/manage.py
+++ backend/manage.py
@@ -1,3 +1,4 @@
+#comment
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
@@ -14,7 +15,5 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
 if __name__ == '__main__':
     main()
